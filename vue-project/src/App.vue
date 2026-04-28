577ad1ad230bcb5d9bdcca980561cfd197d8beb4
wendylin84 <wendyl84@nycstudents.net>
2026-04-28 13:46:41 -0400

    done setting up

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,20 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './supabase'
 
-<template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
+const transactions = ref([])
+const error = ref(null)
+
+onMounted(async () => {
+  let { data: transactionData, error: err } = await supabase.from('transactions').select('*')
+  if (err) {
+    error.value = err.message
+  } else {
+    transactions.value = transactionData
+  }
+})
+</script>
+
+<template></template>
 
 <style scoped></style>
