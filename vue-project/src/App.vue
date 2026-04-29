072a6f5f6071f9f99123f0e57fcda6a760df45d3
wendylin84 <wendyl84@nycstudents.net>
2026-04-29 14:00:33 -0400

    merge

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,15 +2,15 @@
 import { ref, onMounted } from 'vue'
 import { supabase } from './supabase'
 
-const transactions = ref([])
+const posts = ref([])
 const error = ref(null)
 
 onMounted(async () => {
-  let { data: transactionData, error: err } = await supabase.from('transactions').select('*')
+  let { data: postData, error: err } = await supabase.from('posts').select('*')
   if (err) {
     error.value = err.message
   } else {
-    transactions.value = transactionData
+    posts.value = postData
   }
 })
 </script>
