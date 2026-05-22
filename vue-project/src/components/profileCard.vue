a2dc26cd7f96c472342b53ec5b21cb43ce3a3e90
wendylin84 <wendyl84@nycstudents.net>
2026-05-22 14:01:52 -0400

    ts pmo

---
+++ vue-project/src/components/profileCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card">
+    <router-link :to="profilePath">
+      <h2>{{ profiles.username }}</h2>
+    </router-link>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  profile: {
+    type: Object,
+    required: true,
+  },
+  id: {
+    type: Number,
+    required: true,
+  },
+})
+const profilePath = computed(() => {
+  return `/profile/${encodeURIComponent(props.profile.user_id)}`
+})
+</script>
+
+<style scoped>
+.card {
+  width: 28%;
+  height: 300px;
+  background-color: rgb(247, 237, 248);
+  margin: 30px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+}
+</style>
--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import homeView from '@/views/homeView.vue'
 import loginView from '@/views/loginView.vue'
+import registerView from '@/views/registerView.vue'
+import profileView from '@/views/profileView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,6 +15,16 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: loginView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: registerView,
+    },
+    {
+      path: '/profile/:user_id',
+      name: 'profile',
+      component: profileView,
     },
   ],
 })
