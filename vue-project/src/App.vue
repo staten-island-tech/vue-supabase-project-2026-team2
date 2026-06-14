07ed45d726f9513c5962fa5340fbf29966e48566
Edin Gonuler <edingonuler@Edins-MacBook-Pro.local>
2026-06-14 15:54:38 -0400

    j

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,3 +1,42 @@
+<<<<<<< HEAD
+=======
+<template>
+  <!-- <div>
+    <p v-if="error">Error: {{ error }}</p>
+    <div v-for="profile in profiles" :key="profile.id">
+      <p>{{ profile.username }}</p>
+      <p>{{ profile.password }}</p>
+      <p>{{ profile.profilePicture }}</p>
+    </div>
+  </div>
+  <RouterView /> -->
+
+  <div>
+    <p v-if="error">Error: {{ error }}</p>
+
+    <div v-else>
+      <pre>{{ profiles }}</pre>
+
+      <div v-for="profile in profiles" :key="profile.id" class="profile-card">
+        <p>{{ profile }}</p>
+        <p><strong>Username:</strong> {{ profile.username }}</p>
+        <p><strong>Password:</strong> {{ profile.password }}</p>
+        console.log(profile)
+
+        <img
+          v-if="profile.profile_picture"
+          :src="profile.profile_picture"
+          alt="Profile Picture"
+          width="120"
+        />
+      </div>
+    </div>
+
+    <RouterView />
+  </div>
+</template>
+
+>>>>>>> 843e350 (j)
 <script setup>
 import { onMounted, ref } from 'vue'
 import Account from './components/Account.vue'
